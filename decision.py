86df537e8ba191a3aa92afb9f66ef426c2d87d91
Anusharv <anusharathna512@gmail.com>
2024-09-30 22:40:54 -0700

    Update decision.py

--- decision.py
+++ decision.py
@@ -242,7 +242,90 @@ Sample Input:
 20
 L3
 Sample Output:
-L1
+L1'''
+
+Ans:
+
+
+
+'''9)
+There are 3 labs in the CSE department are L1, L2, and L3 with a seating capacity of x, y, and z. A single lab needs to be allocated to a class of 'n' students. How many of the 3 labs can accommodate 'n' students?
+Input format:
+Input consists of 4 integers
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+The fourth input denotes the number of students(x)
+Output format:
+Print the number of labs which can accommodate the 'n' number of students
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+25
+Sample Output:
+2 '''
+
+Ans:
+
+a = int(input())  
+b = int(input())  
+c = int(input()) 
+n = int(input())  
+count = 0
+if a >= n:
+    count += 1
+
+if b >= n:
+    count += 1
+
+if c >= n:
+    count += 1
+print(count)
+
+'''10)
+There are 3 labs in the CSE department. The labs are L1, L2, and L3 with a seating capacity of x, y, and z respectively. A single lab needs to be allocated to a class of 'n' students. The labs need to be utilized to the maximum i.e the number of systems used should not be minimal. Which lab needs to be allocated to this class?
+Input format:
+Input consists of 4 integers
+The first input denotes 'x'
+The second input denotes 'y'
+The third input denotes 'z'
+The fourth input denotes 'n'
+Output format:
+Print the lab which is suitable for  'n' number of students
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+25
+Sample Output:
+L1'''
+
+Ans:
+x = int(input())  # Capacity of L1
+y = int(input())  # Capacity of L2
+z = int(input())  # Capacity of L3
+n = int(input())  # Number of students
+suitable_lab = None
+max_capacity = -1
+if x >= n and x > max_capacity:
+    suitable_lab = "L1"
+    max_capacity = x
+
+if y >= n and y > max_capacity:
+    suitable_lab = "L2"
+    max_capacity = y
+
+if z >= n and z > max_capacity:
+    suitable_lab = "L3"
+    max_capacity = z
+if suitable_lab:
+    print(suitable_lab)
+else:
+    print("No suitable lab available")
+
 
 
 
